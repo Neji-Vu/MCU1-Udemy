62cddfde1551e13d85624ef150259c1bb9ff68f7
Orsted <cuong.vm.21@gmail.com>
2024-10-14 23:27:29 +0700

    update the GPIO Interrupt project with configuring the PLL clock

--- F103_GPIO_Interrupt/Src/main.c
+++ F103_GPIO_Interrupt/Src/main.c
@@ -49,13 +49,18 @@
 #define AFIO_MAPR			(AFIO_BASE_ADDR + 0x04)
 #define AFIO_EXTICR4		(AFIO_BASE_ADDR + 0x14)
 
-#define EXTI				0x40010400
-#define	EXTI_IMR			(EXTI + 0x00)
-#define	EXTI_RTSR			(EXTI + 0x08)
-#define	EXTI_PR				(EXTI + 0x14)
+#define EXTI_BASE_ADDR		0x40010400
+#define	EXTI_IMR			(EXTI_BASE_ADDR + 0x00)
+#define	EXTI_RTSR			(EXTI_BASE_ADDR + 0x08)
+#define	EXTI_PR				(EXTI_BASE_ADDR + 0x14)
 
 // Processor side
 #define NVIC_ISER0			0xE000E100
+
+#define SYSCLK				4U
+#define HSI					5U
+#define HSE					6U
+#define PLLDividedBy2		7U
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
@@ -144,14 +149,36 @@ void ConfigHSEClock(void){
 	*(uint32_t*)RCC_CFGR |= (1U << 0U);
 }
 
-void ConfigPA8AsMCOPin(void){
+void ConfigPLLClockUsingHSE(void){
+	// Turn HSE On
+	*(uint32_t*)RCC_CR |= (1U << 16U);
+	// While until HSE is ready
+	while(!((*(uint32_t*)RCC_CR >> 17U) & 1U));
+
+	// HSE divider for PLL entry (divided by 2)
+	*(uint32_t*)RCC_CFGR |= (1U << 17U);
+	// PLL entry clock source (HSE oscillator clock selected)
+	*(uint32_t*)RCC_CFGR |= (1U << 16U);
+	// PLL multiplication factor (x4)
+	*(uint32_t*)RCC_CFGR |= (2U << 18U);
+
+	// Turn PLL On
+	*(uint32_t*)RCC_CR |= (1U << 24U);
+	// While until PLL is ready
+	while(!((*(uint32_t*)RCC_CR >> 25U) & 1U));
+
+	// Change the system clock into PLL
+	*(uint32_t*)RCC_CFGR |= (2U << 0U);
+}
+
+void ConfigPA8AsMCOPin(uint8_t source){
 	// Set PA8 as MCO pin output
 	*(uint32_t*)GPIOA_CRH &= ~(0xFU << 0U);
 	*(uint32_t*)GPIOA_CRH |= (0x1U << 0U);
 	*(uint32_t*)GPIOA_CRH |= (0x2U << 2U);
 
 	// Select HSE clock to the MCO pin
-	*(uint32_t*)RCC_CFGR |= (0x6U << 24U);
+	*(uint32_t*)RCC_CFGR |= (source << 24U);
 }
 
 int main(void)
@@ -172,12 +199,12 @@ int main(void)
 	// Set interrupt in PC14 line
 	ConfigPC14AsEXTIPin();
 
-	// Config system clock as HSE
-	ConfigHSEClock();
+	// Config system clock as PLL
+	ConfigPLLClockUsingHSE();
 
 	// Set HSE clock to MCO Pin
-	if(((*(uint32_t*)RCC_CFGR >> 2U) & 0x3U) == 0x1){
-		ConfigPA8AsMCOPin();
+	if(((*(uint32_t*)RCC_CFGR >> 2U) & 0x3U) == 0x2){
+		ConfigPA8AsMCOPin(PLLDividedBy2);
 	}
 
     /* Loop forever */
